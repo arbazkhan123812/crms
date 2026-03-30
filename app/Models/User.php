c7f89a57ec9790ea99e1be3fc132000e5f65a890
arbazkhan2025 <arbaznadeem3130@gmail.com>
2026-03-30 10:10:03 +0500

    initial

--- app/Models/User.php
+++ app/Models/User.php
@@ -28,6 +28,8 @@ class User extends Authenticatable
         'password',
     ];
 
+    protected $guarded = ['is_admin'];
+
     /**
      * The attributes that should be hidden for serialization.
      *
